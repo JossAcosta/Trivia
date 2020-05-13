eca3ea400a6f526126d68131c6cab71be4d279bf
jossAcosta <josselineacostamtz@gmail.com>
2020-05-12 19:28:50 -0500

    notifications

--- push.js
+++ push.js
@@ -9,8 +9,11 @@ let vapidKeys =  {
   push.setVapidDetails('mail:josselineacostamtz@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey)
 
   let sub={
-
-  }
+    endpoint:"https://fcm.googleapis.com/fcm/send/cZaR0c-i9hg:APA91bHhvUpNcDHjZljcf3piSBQZnyMmjHlhuCvZI3rHSEvhB145O0Ap9436aOtpr1qS5Rg1WER7Ua_-CVkOPxu4r2ClUOVNkXe8lNNXy66issBvsfbWkVnaN11LxowILnI2uXQ34CVq",
+    expirationTime:null,
+    keys:{
+      p256dh:"BOznonK5AH1DFMN54Uzktb02pnRVISgq6W9YEHSiq6KkMnwqRplquCEsl9ThTZeEMFNuFfz1U-nTuvblD8Cy7GU",
+      auth:"LH3fBUa0BUIUR2VRhxqiyw"}}
 
   push.sendNotification(sub, 'test message')
 
